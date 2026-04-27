227e64af005ebf69fea3e8839218f07a6ecd562a
naty-2450 <118223377+naty-2450@users.noreply.github.com>
2026-04-27 11:21:23 -0300

    Update index.js

--- projeto-shoppe/src/index.js
+++ projeto-shoppe/src/index.js
@@ -6,11 +6,9 @@ const myWhishList = [];
 
 console.log("Welcome to the your Shopee Cart!");
 
-//criando dois itens
-const item1 = await createItem("hotwheels ferrari", 20.99, 1);
-const item2 = await createItem("hotwheels lamborghini", 39.99, 3);
+const item1 = await createItem("book prince cruel", 23.00, 1);
+const item2 = await createItem("manga one piece", 40.00, 3);
 
-// adicionei dois itens ao carrinho
 await cartService.addItem(myCart, item1);
 await cartService.addItem(myCart, item2);
 
@@ -19,7 +17,5 @@ await cartService.removeItem(myCart, item2);
 await cartService.removeItem(myCart, item2);
 
 await cartService.displaycart(myCart);
-// deletei dois itens do carrinho
-// await cartService.deleteItem(myCart, item2.name);
-// await cartService.deleteItem(myCart, item1.name);
+
 await cartService.calculateTotal(myCart);
