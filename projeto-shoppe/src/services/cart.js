2b83339a9e9c33e1308e673018512028db986e3e
naty-2450 <118223377+naty-2450@users.noreply.github.com>
2026-04-27 11:19:45 -0300

    Refactor cart.js by cleaning up comments and logic
    Removed commented-out code and cleaned up the removeItem function logic.

--- projeto-shoppe/src/services/cart.js
+++ projeto-shoppe/src/services/cart.js
@@ -1,12 +1,8 @@
-//quais açoes meu carrinho pode fazer
 
-//CASOS DE USO
-// ✅ -> adicionar item no carrinho
 async function addItem(userCart, item) {
   userCart.push(item);
 }
 
-// ✅ -> calcular o total do carrinho
 async function calculateTotal(userCart) {
   console.log("\nShopee Cart TOTAL IS:");
 
@@ -14,7 +10,6 @@ async function calculateTotal(userCart) {
   console.log(`🎁Total: ${result}`);
 }
 
-// -> deletar item do carrinho
 async function deleteItem(userCart, name) {
   const index = userCart.findIndex((item) => item.name === name);
 
@@ -23,31 +18,27 @@ async function deleteItem(userCart, name) {
   }
 }
 
-// -> ✅ remover um item - diminui um item
 async function removeItem(userCart, item) {
-  //1. encontrar o indice do item
+
   const indexFound = userCart.findIndex((p) => p.name === item.name);
 
-  //2. Caso não encontre o item
   if (indexFound == -1) {
     console.log("item não encontrado");
     return;
   }
 
-  //3. item > 1 subtrair um item
-  if (userCart[indexFound].quantity > 1) {
+
+    if (userCart[indexFound].quantity > 1) {
     userCart[indexFound].quantity -= 1;
     return;
   }
 
-  //4. caso item = 1 deletar o item
   if (userCart[indexFound].quantity == 1) {
     userCart.splice(indexFound, 1);
     return;
   }
 }
 
-// ✅ mostra todos os items do carrinho
 async function displaycart(userCart) {
   console.log("\nShopee cart list:");
   userCart.forEach((item, index) => {
