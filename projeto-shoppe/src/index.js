80a87a6da03dc40f1774061f70b051ad99c5c9d5
Nátaly Vitoria <118223377+naty-2450@users.noreply.github.com>
2026-05-14 08:50:16 -0300

    Add multiple items to the shopping cart
    Added new items to the cart including headset, keyboard, mouse, and an empty item.

--- projeto-shoppe/src/index.js
+++ projeto-shoppe/src/index.js
@@ -8,9 +8,18 @@ console.log("Welcome to the your Shopee Cart!");
 
 const item1 = await createItem("book prince cruel", 23.00, 1);
 const item2 = await createItem("manga one piece", 40.00, 3);
+const item3 = await createItem("headset gamer", 150.00, 1);
+const item4 = await createItem("teclado mecânico", 250.00, 1);
+const item5 = await createItem("mouse", 100.00, 2);
+const item6 = await createItem("", 250.00, 1);
+
 
 await cartService.addItem(myCart, item1);
 await cartService.addItem(myCart, item2);
+await cartService.addItem(myCart, item3);
+await cartService.addItem(myCart, item4);
+await cartService.addItem(myCart, item5);
+await cartService.addItem(myCart, item6);
 
 await cartService.removeItem(myCart, item2);
 await cartService.removeItem(myCart, item2);
